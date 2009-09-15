f645a2ab5c973bfc14c92f56fb0c8dafb79676c9
Corey <corey@seologic.com>
2009-09-15 17:03:59 -0500

    Fixed issue with extra label params bleeding into form fields. Updated docs as well.

--- lib/mir_form_builder.rb
+++ lib/mir_form_builder.rb
@@ -1,53 +1,80 @@
-# Using the Mir Form Builder:
+# == Configuration
+#
+# === Configure your application to default to the custom form builder
 #
 # In app/controllers/application_controller.rb:
 #
 #   class ApplicationController < ActionController::Base
 #     ActionView::Base.default_form_builder = MirFormBuilder
 #
-# In your view:
+# == Usage
 #
-# Default form field with label:
+# === Default form field with label:
 #
 #   <%= f.text_field :last_name -%>
 #
 # Returns:
 #
-#   <fieldset><label for="user_last_name">Last Name</label><br /><input id="user_last_name" name="user[last_name]" size="30" type="text" /></fieldset>
+#   <fieldset>
+#     <label for="user_last_name">Last Name</label><br />
+#     <input id="user_last_name" name="user[last_name]" size="30" type="text" />
+#   </fieldset>
 #
-# Form field with custom label:
+# === Form field with custom label:
 #
 #   <%= f.text_field :first_name, :label => 'Custom' -%>
 #
 # Returns:
 #
-#   <fieldset><label for="user_first_name">Custom</label><br /><input id="user_first_name" label="Custom" name="user[first_name]" size="30" type="text" /></fieldset>
+#   <fieldset>
+#     <label for="user_first_name">Custom</label><br />
+#     <input id="user_first_name" name="user[first_name]" size="30" type="text" />
+#   </fieldset>
 #
-# Form field with inline help (? icon which reveals help content when clicked):
+# === Form field with inline help (? icon which reveals help content when clicked):
 #
-#   <%= f.password_field :password, :help => 'Here are some detailed instructions on valid passwords.' %>
-#
-# Returns:
-#
-#   <fieldset><label for="password">Password: <img src="/images/icons/help_icon.png" onclick="$('password_help').toggle();" class='inline_icon' /></label><br /><div class="inline_help" id="password_help" style="display: none;"><p>Here are some detailed instructions on valid passwords.</p></div><input help="Here are some detailed instructions on valid passwords." id="user_password" name="user[password]" size="30" type="password" /></fieldset>
-#
-# Form field with instructions (show immediately below the label):
-#
-#   <%= f.password_field :password_confirmation, :label => 'Confirm Password', :instructions => 'Enter your password again to confirm.' %>
+#   <%= f.password_field :password %>
 #
 # Returns:
 #
-#   <fieldset><label for="password_confirmation">Confirm Password: <span class="instructions">Enter your password again to confirm.</span></label><br /><input id="user_password_confirmation" instructions="Enter your password again to confirm." label="Confirm Password" name="user[password_confirmation]" size="30" type="password" /></fieldset>
+#   <fieldset>
+#     <label for="password">Password: <img src="/images/icons/help_icon.png"
+#     onclick="$('password_help').toggle();" class='inline_icon' /></label><br />
+#     <div class="inline_help" id="password_help" style="display: none;">
+#       <p>Here are some detailed instructions on valid passwords.</p>
+#     </div>
+#     <input id="user_password" name="user[password]" size="30" type="password" />
+#   </fieldset>
 #
-# Check box with label in addition to checkbox value text (e.g. 'Foo' appears above the checkbox, and 'Something' next to it):
+# === Form field with instructions (show immediately below the label):
+#
+#   <%= f.password_field :password_confirmation %>
+#
+# Returns:
+#
+#   <fieldset>
+#     <label for="password_confirmation">
+#       Confirm Password:
+#       <span class="instructions">Enter your password again to confirm.</span>
+#     </label><br />
+#     <input id="user_password_confirmation" name="user[password_confirmation]" size="30" type="password" />
+#   </fieldset>
+#
+# === Check box with label in addition to checkbox value text
+# (E.g. 'Foo' appears above the checkbox, and 'Something' next to it):
 #
 #   <%= f.check_box :foo, :inline_label => 'Something' -%>
 #
 # Returns:
 #
-#   <fieldset><label for="user_foo">Foo</label><br /><input name="user[foo]" type="hidden" value="0" /><input id="user_foo" inline_label="Something" name="user[foo]" type="checkbox" value="1" /><label class="inline" for="user_foo">Something</label><br style='clear: both;'/><br /></fieldset>
+#   <fieldset>
+#     <label for="user_foo">Foo</label><br />
+#     <input name="user[foo]" type="hidden" value="0" />
+#     <input id="user_foo" name="user[foo]" type="checkbox" value="1" />
+#     <label class="inline" for="user_foo">Something</label><br style='clear: both;'/><br />
+#   </fieldset>
 #
-# Troubleshooting
+# == Troubleshooting
 #
 # If you're seeing double form labels, it's because you still have <%= label -%> elements in your forms.
 #
@@ -73,6 +100,9 @@ class MirFormBuilder < ActionView::Helpers::FormBuilder
         _label = label(field, _label_text) + "<br />"
       end
       _inline_label = options[:inline_label] ? label(field, options[:inline_label], :class => 'inline') + "<br style='clear: both;'/><br />" : ""
+      options.delete(:instructions)
+      options.delete(:help)
+      options.delete(:inline_label)
       @template.content_tag(:fieldset, _label + super + _inline_label)
     end
   
