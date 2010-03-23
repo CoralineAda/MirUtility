1f789b94a9afbd32f8690f4f519f28970c1bb858
Roderick Monje <oblomov@localhost.(none)>
2010-03-23 15:24:37 -0500

    rev'd to 0.3.10: added sanitize-params methods
    moved spec.html to doc; ignored doc
    forced environment to test
    removed unused dev/prod/logs config

--- config/initializers/logs.rb
+++
@@ -1,7 +0,0 @@
-# Sets up logging for 404 errors. Usage:
-
-# ERROR_404_LOG.warn request.url
-
-_file = File.open( "#{RAILS_ROOT}/log/error_404.log", 'a' )
-_file.sync = true
-ERROR_404_LOG = Logger.new(_file)
--- lib/mir_utility.rb
+++ lib/mir_utility.rb
@@ -63,6 +63,29 @@ module MirUtility
 
 end
 
+module ActionController
+  # Returns a sanitized column parameter suitable for SQL order-by clauses.
+  def sanitize_by_param(allowed=[], default='id')
+    sanitize_params params[:by], allowed, default
+  end
+
+  # Returns a sanitized direction parameter suitable for SQL order-by clauses.
+  def sanitize_dir_param
+    sanitize_params params[:dir], ['ASC', 'DESC'], 'ASC'
+  end
+
+  # Use this method to prevent SQL injection vulnerabilityes by verifying that a user-provided
+  # parameter is on a whitelist of allowed values.
+  #
+  # Accepts a value, a list of allowed values, and a default value.
+  # Returns the value if allowed, otherwise the default.
+  def sanitize_params(supplied='', allowed=[], default=nil)
+    raise ArgumentError, "A default value is required." unless default
+    return default if supplied.blank? || allowed.blank? || ! allowed.include?(supplied)
+    return supplied
+  end
+end
+
 module ActiveRecord::Validations::ClassMethods
 
   # Overrides validates associates.
@@ -719,6 +742,7 @@ end
 class TrueClass
   def to_i; 1; end
 end
+
 class FalseClass
   def to_i; 0; end
 end
