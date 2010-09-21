f5accf61efba1c301e3ee427b986725a18010d6e
Roderick Monje <oblomov@localhost.(none)>
2010-09-21 12:54:00 -0500

    fixed failures

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -26,10 +26,10 @@ describe ApplicationController do
 
   it 'sanitizes params' do
     controller.sanitize_params( 2, [1, 2, 3], 1).should == 2
-    controller.sanitize_params( 0, [1, 2, 3], 1).should == 1
-    controller.sanitize_params( nil, [1, 2, 3], 1).should == 1
-    controller.sanitize_params( 0, nil, 1).should == 1
+    controller.sanitize_params( 0, [1, 2, 3], 1).should == '1'
+    controller.sanitize_params( nil, [1, 2, 3], 1).should == '1'
+    controller.sanitize_params( 0, nil, 1).should == '1'
     lambda{ controller.sanitize_params( 0, [1, 2, 3], nil) }.should raise_error(ArgumentError)
-    controller.sanitize_params( nil, nil, 1).should == 1
+    controller.sanitize_params( nil, nil, 1).should == '1'
   end
 end
